bdae084f6927cb045af6a7189ae2b60a659e9627
Ryan McManus <ryancmcmanus@gmail.com>
2017-06-23 17:30:10 -0400

    add change mind request

---
+++ app/src/main/java/com/moviepass/requests/ChangedMindRequest.java
@@ -0,0 +1,8 @@
+package com.moviepass.requests;
+
+/**
+ * Created by anubis on 6/20/17.
+ */
+
+public class ChangedMindRequest {
+}
---
+++ app/src/main/java/com/moviepass/responses/ChangedMindResponse.java
@@ -0,0 +1,8 @@
+package com.moviepass.responses;
+
+/**
+ * Created by anubis on 6/20/17.
+ */
+
+public class ChangedMindResponse {
+}
