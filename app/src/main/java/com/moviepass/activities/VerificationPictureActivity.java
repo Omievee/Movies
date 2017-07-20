d3dbdfc90b5758e0d5fd89840c98b8e7b4bf5efa
Ryan McManus <ryancmcmanus@gmail.com>
2017-07-19 22:05:55 -0400

    continue ticket verification

--- app/src/main/java/com/moviepass/activities/VerificationPictureActivity.java
+++ app/src/main/java/com/moviepass/activities/VerificationPictureActivity.java
@@ -5,16 +5,25 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.graphics.Rect;
 import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -24,6 +33,7 @@ import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.google.firebase.crash.FirebaseCrash;
 import com.moviepass.R;
 import com.moviepass.UserPreferences;
 import com.moviepass.application.Application;
@@ -44,8 +54,10 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -55,7 +67,7 @@ import retrofit2.Response;
  * Created by anubis on 7/17/17.
  */
 
-public class VerificationPictureActivity extends AppCompatActivity {
+public class VerificationPictureActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
     static {
         System.loadLibrary("native-lib");
@@ -71,6 +83,15 @@ public class VerificationPictureActivity extends AppCompatActivity {
     private boolean isActivityFinished;
 
     private Camera camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+
+    private ImageView mRequestFocusImageView;
+    private RelativeLayout mRelFocusViewContainer;
+    private final int FOCUS_AREA_SIZE = 400;
+
+    private Handler handler;
+    private Animation startFadeOutAnimation;
 
     private AmazonS3 s3;
     private TransferUtility transferUtility;
@@ -88,20 +109,213 @@ public class VerificationPictureActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification_picture);
-
+        
         s3 = ((Application) getApplicationContext()).getAmazonS3Client();
         transferUtility = new TransferUtility(s3, getApplicationContext());
 
         Bundle extras = getIntent().getExtras();
         token = Parcels.unwrap(getIntent().getParcelableExtra(TOKEN));
 
+        handler = new Handler();
+        surfaceView = findViewById(R.id.surface_view);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+
         buttonTakePicture = findViewById(R.id.take_picture);
         buttonRetakePicture = findViewById(R.id.retake_picture);
         buttonSubmitPicture = findViewById(R.id.submit_picture);
 
+        mRequestFocusImageView = findViewById(R.id.image_focus_touch);
+        mRelFocusViewContainer = findViewById(R.id.focus_view_container);
+        
         progress = findViewById(R.id.progress);
+        startFadeOutAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
+
+        buttonTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
     }
 
+    @Override
+    public void onPause() {
+        surfaceView.setVisibility(View.GONE);
+        if (camera != null) {
+            releaseCamera();
+        }
+
+        if (this.isFinishing()) {
+            isActivityFinished = true;
+        } else {
+            isActivityFinished = false;
+        }
+
+        super.onPause();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        if (camera != null) {
+            releaseCamera();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (this.camera == null) {
+            try {
+                this.camera = Camera.open();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            surfaceView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void releaseCamera() {
+        if (camera != null) {
+
+            isPreview = false;
+            camera.stopPreview();
+            camera.lock();
+            camera.setPreviewCallback(null);
+            camera.release();
+            camera = null;
+        }
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            setCameraDisplayOrientation(this, 0, camera);
+
+            Camera.Parameters p = camera.getParameters();
+            List<String> focusModes = p.getSupportedFocusModes();
+            if (p.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+                p.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            }
+            if (p.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+                p.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+            }
+            camera.setParameters(p);
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+            if (focusModes != null && focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+                camera.autoFocus(mAutoFocusTakePictureCallback);
+            }
+            addViewAndRemove(holder.getSurfaceFrame().centerX(),holder.getSurfaceFrame().centerY());
+            isPreview = true;
+        } catch (IOException e) {
+            //Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+        // stop preview before making changes
+        try {
+            stopPreview();
+        } catch (Exception e) {
+            // ignore: tried to stop a non-existent preview
+        }
+    }
+
+    /**
+     * When this function returns, camera will be null.
+     */
+    public void stopPreview() {
+        if (camera != null) {
+
+            isPreview = false;
+            camera.stopPreview();
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (holder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+
+            isPreview = false;
+            camera.stopPreview();
+        } catch (Exception e) {
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        // start preview with new settings
+        try {
+            Camera.Parameters p = camera.getParameters();
+            Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+            if (display.getRotation() == Surface.ROTATION_0) {
+                camera.setDisplayOrientation(90);
+            }
+
+            if (display.getRotation() == Surface.ROTATION_270) {
+                camera.setDisplayOrientation(180);
+            }
+
+            setCameraDisplayOrientation(this, 0, camera);
+
+            if (p.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+                p.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            }
+            if (p.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+                p.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+            }
+            camera.setParameters(p);
+
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+
+            isPreview = true;
+        } catch (Exception e) {
+            //Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    public static void setCameraDisplayOrientation(Activity activity,
+                                                   int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int result;
+
+        result = (info.orientation - degrees + 360) % 360;
+
+        camera.setDisplayOrientation(result);
+    }
 
     void addViewAndRemove(float x, float y) {
         mRelFocusViewContainer.removeAllViews();
@@ -197,6 +411,12 @@ public class VerificationPictureActivity extends AppCompatActivity {
         } catch (Exception e) {
 
         }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
     }
 
     private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
@@ -347,7 +567,7 @@ public class VerificationPictureActivity extends AppCompatActivity {
                         RestClient.getAuthenticated().verifyTicket(reservationId, ticketVerificationRequest).enqueue(new Callback<VerificationResponse>() {
                             @Override
                             public void onResponse(Call<VerificationResponse> call, Response<VerificationResponse> response) {
-                                Intent confirmationIntent = new Intent(VerificationPictureActivity.this, VerificationConfirmationActivity.class);
+                                Intent confirmationIntent = new Intent(VerificationPictureActivity.this, BrowseActivity.class);
                                 startActivity(confirmationIntent);
                                 finish();
                             }
@@ -363,7 +583,7 @@ public class VerificationPictureActivity extends AppCompatActivity {
                         RestClient.getAuthenticated().verifyTicket(reservationId, ticketVerificationRequest).enqueue(new Callback<VerificationResponse>() {
                             @Override
                             public void onResponse(Call<VerificationResponse> call, Response<VerificationResponse> response) {
-                                Intent confirmationIntent = new Intent(VerificationPictureActivity.this, VerificationConfirmationActivity.class);
+                                Intent confirmationIntent = new Intent(VerificationPictureActivity.this, BrowseActivity.class);
                                 startActivity(confirmationIntent);
                                 finish();
                             }
@@ -436,6 +656,55 @@ public class VerificationPictureActivity extends AppCompatActivity {
                 timeStamp + ".jpg");
 
         return mediaFile;
+    }
+
+    private void focusOnTouch(MotionEvent event) {
+        if (camera != null) {
+            try {
+                Camera.Parameters parameters = camera.getParameters();
+                List<String> focusModes = parameters.getSupportedFocusModes();
+                if (parameters.getMaxNumMeteringAreas() > 0) {
+                    Log.i(TAG, "fancy !");
+                    Rect rect = calculateFocusArea(event.getX(), event.getY());
+
+                    parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+                    List<Camera.Area> meteringAreas = new ArrayList<Camera.Area>();
+                    meteringAreas.add(new Camera.Area(rect, 1000));
+                    parameters.setFocusAreas(meteringAreas);
+
+                    camera.setParameters(parameters);
+                    camera.autoFocus(mAutoFocusTakePictureCallback);
+                } else {
+                    if (focusModes != null && focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+                        camera.autoFocus(mAutoFocusTakePictureCallback);
+                    }                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                FirebaseCrash.report(new Exception(e.getMessage()));
+            }
+            addViewAndRemove(event.getX(), event.getY());
+        }
+    }
+
+    private Rect calculateFocusArea(float x, float y) {
+        int left = clamp(Float.valueOf((x / surfaceView.getWidth()) * 2000 - 1000).intValue(), FOCUS_AREA_SIZE);
+        int top = clamp(Float.valueOf((y / surfaceView.getHeight()) * 2000 - 1000).intValue(), FOCUS_AREA_SIZE);
+
+        return new Rect(left, top, left + FOCUS_AREA_SIZE, top + FOCUS_AREA_SIZE);
+    }
+
+    private int clamp(int touchCoordinateInCameraReper, int focusAreaSize) {
+        int result;
+        if (Math.abs(touchCoordinateInCameraReper) + focusAreaSize / 2 > 1000) {
+            if (touchCoordinateInCameraReper > 0) {
+                result = 1000 - focusAreaSize / 2;
+            } else {
+                result = -1000 + focusAreaSize / 2;
+            }
+        } else {
+            result = touchCoordinateInCameraReper - focusAreaSize / 2;
+        }
+        return result;
     }
 
     private Camera.AutoFocusCallback mAutoFocusTakePictureCallback = new Camera.AutoFocusCallback() {
