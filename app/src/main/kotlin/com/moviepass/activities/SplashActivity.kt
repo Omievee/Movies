bc569da21af750b167a5025e784c224e90c431cc
Ryan McManus <ryancmcmanus@gmail.com>
2017-08-04 17:29:21 -0400

    change first open redirect logic

--- app/src/main/kotlin/com/moviepass/activities/SplashActivity.kt
+++ app/src/main/kotlin/com/moviepass/activities/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 import com.moviepass.R
 import com.moviepass.UserPreferences
@@ -15,16 +16,16 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-//            if (UserPreferences.getHasUserLoggedInBefore() || UserPreferences.getUserId() != 0) {
-/*            if (UserPreferences.getUserId() != 0) {
-                val i = Intent(this@SplashActivity, BrowseActivity::class.java)
-                startActivity(i)
-                finish()
-            } else { */
-                val i = Intent(this@SplashActivity, OnboardingActivity::class.java)
-                startActivity(i)
-                finish()
-//            }
+                Log.d("userId", UserPreferences.getUserId().toString())
+                if (UserPreferences.getUserId() == 0 || UserPreferences.getUserId().equals("")) {
+                    val i = Intent(this@SplashActivity, OnboardingActivity::class.java)
+                    startActivity(i)
+                    finish()
+                } else {
+                    val i = Intent(this@SplashActivity, BrowseActivity::class.java)
+                    startActivity(i)
+                    finish()
+                }
         }, SPLASH_TIME_OUT.toLong())
     }
 
