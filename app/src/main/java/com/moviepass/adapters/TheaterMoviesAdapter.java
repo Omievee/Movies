ba392f6bca238f5446689e58b920d86961765d63
Omar Vicarra <omar@moviepass.com>
2017-11-30 13:28:53 -0500

    xml attributes added / modified

--- app/src/main/java/com/moviepass/adapters/TheaterMoviesAdapter.java
+++ app/src/main/java/com/moviepass/adapters/TheaterMoviesAdapter.java
@@ -1,9 +1,7 @@
 package com.moviepass.adapters;
 
 import android.content.Context;
-import android.graphics.drawable.Animatable;
 import android.net.Uri;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -13,10 +11,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.controller.BaseControllerListener;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
-import com.facebook.imagepipeline.image.ImageInfo;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
 import com.moviepass.R;
@@ -24,6 +20,7 @@ import com.moviepass.listeners.ScreeningPosterClickListener;
 import com.moviepass.model.Screening;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,7 +33,7 @@ public class TheaterMoviesAdapter extends RecyclerView.Adapter<TheaterMoviesAdap
     public static final String TAG = "found";
     private final ScreeningPosterClickListener screeningPosterClickListener;
     private ArrayList<Screening> screeningsArrayList;
-
+    List<String> startTimes;
     private final int TYPE_ITEM = 0;
     private LayoutInflater inflater;
     private Context context;
@@ -54,20 +51,23 @@ public class TheaterMoviesAdapter extends RecyclerView.Adapter<TheaterMoviesAdap
         TextView cinemaTItle;
         @BindView(R.id.CINEMAPOSTER)
         SimpleDraweeView cinemaPoster;
+        @BindView(R.id.CINEMA_SHOWTIMES)
+        RecyclerView cinemaShowtimesRecycler;
 
         public ViewHolder(View v) {
             super(v);
             ButterKnife.bind(this, v);
-
             cinemaCardViewListItem = v.findViewById(R.id.list_item_cinemaposterCARDVIEW);
             cinemaTItle = v.findViewById(R.id.cinema_movieTitle);
             cinemaPoster = v.findViewById(R.id.CINEMAPOSTER);
+            cinemaShowtimesRecycler = v.findViewById(R.id.CINEMA_SHOWTIMES);
         }
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_cinemaposter, parent, false);
+
         return new ViewHolder(view);
     }
 
@@ -76,6 +76,7 @@ public class TheaterMoviesAdapter extends RecyclerView.Adapter<TheaterMoviesAdap
     public void onBindViewHolder(final ViewHolder holder, int position) {
         final Screening screening = screeningsArrayList.get(position);
         Log.d(TAG, "first titles: " + screening.getTitle());
+        startTimes = screening.getStartTimes();
 
         final Uri imgUrl = Uri.parse(screening.getImageUrl());
         Log.d(TAG, "onBindViewHolder: " + imgUrl.toString());
@@ -93,41 +94,21 @@ public class TheaterMoviesAdapter extends RecyclerView.Adapter<TheaterMoviesAdap
         holder.cinemaPoster.setImageURI(imgUrl);
         holder.cinemaPoster.getHierarchy().setFadeDuration(500);
         holder.cinemaTItle.setText(screening.getTitle());
-
         holder.cinemaPoster.setController(controller);
 
-//        if (imgUrl.isEmpty()) {
-//            Picasso.Builder builder = new Picasso.Builder(holder.itemView.getContext());
-//            builder.listener(new Picasso.Listener() {
-//                @Override
-//                public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
-//                    exception.printStackTrace();
-//
-//                    holder.title.setText(screening.getTitle());
-//                }
-//            });
-//            builder.build()
-//                    .load(R.drawable.ticket_top_red_dark)
-//                    .placeholder(R.drawable.ticket_top_red_dark)
-//                    .error(R.drawable.ticket_top_red_dark)
-//                    .into(holder.posterImageView);
-//            holder.title.setText(screening.getTitle());
-//        } else {
-//            Picasso.Builder builder = new Picasso.Builder(holder.itemView.getContext());
-//            builder.listener(new Picasso.Listener() {{
-//                @Override
-//                public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
-//                    exception.printStackTrace();
-//
-//                    holder.title.setText(screening.getTitle());
-//                }
-//            });
-//            builder.build()
-//                    .load(imgUrl)
-//                    .placeholder(R.drawable.ticket_top_red_dark)
-//                    .error(R.drawable.ticket_top_red_dark)
-//                    .into(holder.posterImageView);
-//        }
+        holder.cinemaCardViewListItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (holder.cinemaShowtimesRecycler.getVisibility() == View.GONE) {
+                    holder.cinemaShowtimesRecycler.setVisibility(View.VISIBLE);
+                }
+
+//                screeningPosterClickListener.onScreeningPosterClick(holder.getAdapterPosition(), screening, startTimes, holder.cinemaPoster);
+
+            }
+        });
+
 
 //        holder.listItemMoviePoster.setTag(position);
 //
@@ -157,7 +138,6 @@ public class TheaterMoviesAdapter extends RecyclerView.Adapter<TheaterMoviesAdap
 //                    }, 150);
 //                }
 //
-//                screeningPosterClickListener.onScreeningPosterClick(holder.getAdapterPosition(), screening, startTimes, holder.posterImageView);
 //            }
 //        });
     }
