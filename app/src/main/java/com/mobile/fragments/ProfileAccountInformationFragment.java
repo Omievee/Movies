97420a91d8f64a48145d08c3cea89b6edaa19769
Ivonne <ivonne@adminrethinkits-MacBook-Pro.local>
2018-03-15 12:10:12 -0400

    committing before swithing branches

--- app/src/main/java/com/mobile/fragments/ProfileAccountInformationFragment.java
+++ app/src/main/java/com/mobile/fragments/ProfileAccountInformationFragment.java
@@ -18,6 +18,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
 import android.view.inputmethod.InputMethodManager;
