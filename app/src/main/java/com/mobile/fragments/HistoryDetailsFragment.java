b20f5c0e00972d281f4e0bd8b4e34d55fa22d117
Omar Vicarra <omar@moviepass.com>
2018-03-27 14:18:09 -0400

    Blurred background added.. adjusting fragment overlaying

--- app/src/main/java/com/mobile/fragments/HistoryDetailsFragment.java
+++ app/src/main/java/com/mobile/fragments/HistoryDetailsFragment.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.drawable.Animatable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.transition.TransitionInflater;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,17 +23,20 @@ import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.image.ImageInfo;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.mobile.Constants;
 import com.mobile.model.Movie;
 import com.moviepass.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import jp.wasabeef.blurry.Blurry;
+
 /**
  * Created by o_vicarra on 3/27/18.
  */
 
-public class HistoryDetailsFragment extends Fragment {
+public class HistoryDetailsFragment extends DialogFragment {
 
     private static final String HISTORY_POSTER = "poster";
     private static final String EXTRA_TRANSITION_NAME = "transition_name";
@@ -39,6 +44,7 @@ public class HistoryDetailsFragment extends Fragment {
     Context myContext;
     SimpleDraweeView enlargedImage;
     TextView historyDate, historyTitle, historyLocal;
+
     public HistoryDetailsFragment() {
     }
 
@@ -71,7 +77,7 @@ public class HistoryDetailsFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         myActivity.startPostponedEnterTransition();
-        setSharedElementEnterTransition(TransitionInflater.from(myActivity).inflateTransition(android.R.transition.explode));
+        setSharedElementEnterTransition(TransitionInflater.from(myActivity).inflateTransition(android.R.transition.move).setDuration(4000));
     }
 
 
@@ -81,6 +87,8 @@ public class HistoryDetailsFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fr_historydetails, container, false);
 
+
+        Blurry.with(myActivity).radius(25).sampling(2).onto(container);
 
         return root;
     }
@@ -98,6 +106,8 @@ public class HistoryDetailsFragment extends Fragment {
         String transition = getArguments().getString(EXTRA_TRANSITION_NAME);
 
         Uri imgUrl = Uri.parse(historyItem.getImageUrl());
+
+
         ImageRequest request = ImageRequestBuilder.newBuilderWithSource(imgUrl)
                 .setProgressiveRenderingEnabled(true)
                 .setSource(imgUrl)
@@ -111,21 +121,33 @@ public class HistoryDetailsFragment extends Fragment {
                     public void onFinalImageSet(String id, @Nullable ImageInfo imageInfo, @Nullable Animatable animatable) {
                         super.onFinalImageSet(id, imageInfo, animatable);
 
+                        enlargedImage.setImageURI(imgUrl);
                     }
 
                     @Override
                     public void onFailure(String id, Throwable throwable) {
+                        if (historyItem.getImageUrl().contains("https://s3.amazonaws.com/")) {
+                            enlargedImage.setImageURI(imgUrl + "/original.jpg");
+                        }
                     }
                 })
                 .build();
 
+        Log.d(Constants.TAG, "MOVIE POSTER????: " + historyItem.getImageUrl());
+
         long createdAt = historyItem.getCreatedAt();
-        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        SimpleDateFormat sdf = new SimpleDateFormat("M/dd/yyyy");
         historyDate.setText(sdf.format(new Date(createdAt)));
 
         historyLocal.setText(historyItem.getTheaterName());
         historyTitle.setText(historyItem.getTitle());
         enlargedImage.setTransitionName(transition);
         enlargedImage.setController(controller);
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        myActivity.getFragmentManager().popBackStack();
     }
 }
