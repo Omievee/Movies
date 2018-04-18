117dadf1cc80968084c2d523ef2f5aa080e8b60a
Omie <omar@moviepass.com>
2018-04-18 11:38:09 -0400

    confirm button disabled

--- app/src/main/java/com/mobile/fragments/SignUpStepThreeFragment.java
+++ app/src/main/java/com/mobile/fragments/SignUpStepThreeFragment.java
@@ -7,11 +7,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import com.helpshift.support.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -20,6 +18,7 @@ import com.braintreepayments.api.interfaces.BraintreeCancelListener;
 import com.braintreepayments.api.interfaces.BraintreeErrorListener;
 import com.braintreepayments.api.interfaces.PaymentMethodNonceCreatedListener;
 import com.braintreepayments.api.models.PaymentMethodNonce;
+import com.helpshift.support.Log;
 import com.mobile.activities.SignUpActivity;
 import com.mobile.model.ProspectUser;
 import com.mobile.network.RestClient;
@@ -110,8 +109,8 @@ public class SignUpStepThreeFragment extends Fragment implements PaymentMethodNo
 
         //CHANGING PAYMENT DISCLAIMER
 //        paymentDisclaimer.setText(ProspectUser.plan.getPaymentDisclaimer());
-        paymentDisclaimer.setText(getResources().getString(R.string.fragment_sign_up_step_three_bottom_disclaimer_part_one)+ " "+ProspectUser.plan.getConfirmTotal()+" "+
-                getResources().getString(R.string.fragment_sign_up_step_three_bottom_disclaimer_part_two)+" "+getResources().getString(R.string.fragment_sign_up_step_three_bottom_disclaimer_part_three));
+        paymentDisclaimer.setText(getResources().getString(R.string.fragment_sign_up_step_three_bottom_disclaimer_part_one) + " " + ProspectUser.plan.getConfirmTotal() + " " +
+                getResources().getString(R.string.fragment_sign_up_step_three_bottom_disclaimer_part_two) + " " + getResources().getString(R.string.fragment_sign_up_step_three_bottom_disclaimer_part_three));
 
 
         return rootview;
@@ -169,9 +168,9 @@ public class SignUpStepThreeFragment extends Fragment implements PaymentMethodNo
         String gender = ProspectUser.gender;
         String selectedPlanId;
 
-        if (ProspectUser.plan == null)
+        if (ProspectUser.plan == null) {
             selectedPlanId = null;
-        else {
+        } else {
             selectedPlanId = ProspectUser.plan.getId();
         }
 
