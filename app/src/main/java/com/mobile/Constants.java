996aa0caccc086636ce36ed66dd3c2ed43d10f54
Ryan Gravener <rgravener@moviepass.com>
2018-05-16 17:26:54 -0400

    add day of year that users theaters was updated;

--- app/src/main/java/com/mobile/Constants.java
+++ app/src/main/java/com/mobile/Constants.java
@@ -27,6 +27,7 @@ public class Constants {
     public static final String SEAT = "seat";
     public static final String DEVICE_ID = "ID";
     public static final String ONE_DEVICE_ID = "ODID";
+    public static final String LAST_DOWNLOADED_THEATERS = "last_dl_theaters";
     //RESTRICTIONS
     public static final String ACTIVE = "ACTIVE";
     public static final String ACTIVE_FREE_TRIAL = "ACTIVE_FREE_TRIAL";
