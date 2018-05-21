88ec512066bacd392f10f6eb23311331193585b0
Ryan Gravener <rgravener@moviepass.com>
2018-05-21 08:17:47 -0400

    add History qualifier

---
+++ app/src/main/java/com/mobile/history/History.kt
@@ -0,0 +1,11 @@
+package com.mobile.history
+
+import javax.inject.Qualifier
+import javax.inject.Scope
+
+@Qualifier
+@MustBeDocumented
+@Retention(value = AnnotationRetention.RUNTIME)
+annotation class History {
+
+}
