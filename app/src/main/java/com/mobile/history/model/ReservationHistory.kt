7ff3c34da10679a7710bccabd901849e31cc7bdc
Ryan Gravener <rgravener@moviepass.com>
2018-06-21 15:34:30 -0400

    don't use a date here since realm is saving as the wrong value for some reason

--- app/src/main/java/com/mobile/history/model/ReservationHistory.kt
+++ app/src/main/java/com/mobile/history/model/ReservationHistory.kt
@@ -2,31 +2,43 @@ package com.mobile.history.model
 
 import android.os.Parcelable
 import io.realm.RealmObject
+import io.realm.annotations.Ignore
+import io.realm.annotations.PrimaryKey
 import io.realm.annotations.RealmClass
+import kotlinx.android.parcel.IgnoredOnParcel
 import kotlinx.android.parcel.Parcelize
 import java.util.*
 
 @RealmClass
 @Parcelize
 open class ReservationHistory(
+        @PrimaryKey
         var id: Int? = null,
         var title: String? = null,
         var titleNormalized: String? = null,
         var imageUrl: String? = null,
         var userRating: String? = null,
         var theaterName: String? = null,
-        var createdAt: Date? = null,
+        var createdAt: Long? = null,
         var updatedAt: Date = Date()
 
 ) : RealmObject(), Parcelable {
 
-    val rating: Rating
+    @IgnoredOnParcel val rating: Rating
         get() {
             return Rating.values().find {
                 it.name.toUpperCase() == userRating?.toUpperCase()
             } ?: Rating.UNKNOWN
         }
 
+   @delegate:Ignore
+   @IgnoredOnParcel val created by lazy {
+        val createdAt = createdAt
+        when(createdAt) {
+            null->null
+            else->Date(createdAt)
+        }
+    }
 }
 
 enum class Rating {
