e5fc4261608ccaefb57d7464376b6732cc21d100
Ryan Gravener <rgravener@moviepass.com>
2018-08-06 13:07:59 -0400

    AVO-1217 fix the interstitial screen

--- app/src/debug/java/com/moviepass/debug/DebugActivity.kt
+++ app/src/debug/java/com/moviepass/debug/DebugActivity.kt
@@ -2,15 +2,12 @@ package com.moviepass.debug
 
 import android.os.Bundle
 import com.mobile.MPActivty
-import com.mobile.model.Availability
-import com.mobile.model.Screening
-import com.mobile.model.Surge
-import com.mobile.model.SurgeType
-import com.mobile.screening.ScreeningPresentation
+import com.mobile.fragments.AlertScreenFragment
+import com.mobile.model.*
 import com.mobile.seats.SeatPreviewListener
+import com.mobile.utils.showFragment
 import com.moviepass.R
 import dagger.android.AndroidInjection
-import kotlinx.android.synthetic.debug.activity_debug.*
 
 class DebugActivity : MPActivty(), SeatPreviewListener {
 
@@ -22,14 +19,17 @@ class DebugActivity : MPActivty(), SeatPreviewListener {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_debug)
-        val screening = Screening(approved = true)
-        showtime.bind(
-                Availability(startTime = "3:00 PM"),
-                surge = Surge(level = SurgeType.SURGING),
-                screening = ScreeningPresentation(
-                        screening = screening,
-                        selected = android.util.Pair(screening, "3:00 PM")
-                )
-        )
+        showFragment(AlertScreenFragment.newInstance(Alert(
+                id = "foo",
+                title = "A title that spans 1 if not 2 lines okie man i see",
+                body = """As of August 15th, 2018 we are introducing a new pricing plan for all of our members that retains the features you love the most and removes the ones you don't. See up to three standard movies a month for $9.95 and get up to a $5.00 discount to any additional movie tickets purchased.
+
+By clicking the 'I Accept' button below, you are choosing to remain a MoviePass subscriber under these new terms and will be charged $9.95 when your account renews on MM/DD/YY. If you do not wish to accept at this time, you may dismiss this message.""",
+                urlTitle="Learn More",
+                url = "https://moviepass.com/service_updates_20180727",
+                dismissButton = true,
+                dismissButtonText = "I Agree",
+                dismissButtonWebhook = "http://google.com"
+        )))
     }
 }
