a237be764551be607570597ec26686640bf977e7
Ryan Gravener <rgravener@moviepass.com>
2018-09-10 09:53:31 -0400

    make isEmulator a lazy var to optimize

--- app/src/main/java/com/mobile/MPActivty.kt
+++ app/src/main/java/com/mobile/MPActivty.kt
@@ -19,17 +19,16 @@ open class MPActivty : FragmentActivity(), HasSupportFragmentInjector {
     }
 
     companion object {
-        val isEmulator: Boolean
-            get() {
-                return BuildConfig.DEBUG && (Build.FINGERPRINT.startsWith("generic")
-                        || Build.FINGERPRINT.startsWith("unknown")
-                        || Build.MODEL.contains("google_sdk")
-                        || Build.MODEL.contains("Emulator")
-                        || Build.MODEL.contains("Android SDK built for x86")
-                        || Build.MANUFACTURER.contains("Genymotion")
-                        || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
-                        || "google_sdk" == Build.PRODUCT)
-            }
+        val isEmulator: Boolean by lazy {
+            BuildConfig.DEBUG && (Build.FINGERPRINT.startsWith("generic")
+                    || Build.FINGERPRINT.startsWith("unknown")
+                    || Build.MODEL.contains("google_sdk")
+                    || Build.MODEL.contains("Emulator")
+                    || Build.MODEL.contains("Android SDK built for x86")
+                    || Build.MANUFACTURER.contains("Genymotion")
+                    || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
+                    || "google_sdk" == Build.PRODUCT)
+        }
     }
 
 }
