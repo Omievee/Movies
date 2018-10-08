1f65cde62d107595e2a7f5aa0954688e238618ce
Ivonne Ortega <36705474+ivonneortega28@users.noreply.github.com>
2018-10-08 16:49:56 -0400

    Featured View crash fix for Lollipop 5.0

--- app/src/main/java/com/mobile/movie/FeaturedView.kt
+++ app/src/main/java/com/mobile/movie/FeaturedView.kt
@@ -2,7 +2,6 @@ package com.mobile.movie
 
 import android.content.Context
 import android.os.Build
-import android.support.annotation.RequiresApi
 import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.util.AttributeSet
@@ -17,34 +16,34 @@ import android.support.v7.widget.RecyclerView
 import com.mobile.Constants
 import com.mobile.featured.FeaturedMovieAdapter
 import com.mobile.featured.IndicatorsRecyclerViewItemDecoration
+import com.mobile.model.Movie
 
 class FeaturedView(context: Context?, attrs: AttributeSet? = null) : ConstraintLayout(context, attrs) {
 
     var adapter: FeaturedMovieAdapter? = null
     var currentView: View? = null
     var linearLayout: LinearLayoutManager? = null
+    var snapHelper:  PagerSnapHelper ? = null
 
     init {
         View.inflate(context, R.layout.layout_featured_container, this)
         layoutParams = MarginLayoutParams(MATCH_PARENT, WRAP_CONTENT)
+        linearLayout = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        trailerView.layoutManager = linearLayout
+        snapHelper = PagerSnapHelper()
+        snapHelper?.attachToRecyclerView(trailerView)
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
     fun bind(presentation: MoviesPresentation, moviePosterClickListener: MoviePosterClickListener) {
         setUpAdapter(presentation, moviePosterClickListener)
         trailerView.adapter = adapter
-        linearLayout = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        trailerView.layoutManager = linearLayout
-        val snapHelper = PagerSnapHelper()
-        snapHelper.attachToRecyclerView(trailerView)
-        setUpAdapter(presentation,moviePosterClickListener)
 
         val endlessScrollSize = presentation.data.second.size
         when (endlessScrollSize > 1) {
             true -> {
                 val scrollToPosition = endlessScrollSize * Constants.ENDLESS_HALF_START_POSITION
                 trailerView.addItemDecoration(IndicatorsRecyclerViewItemDecoration(endlessScrollSize))
-                setUpScrollListener(snapHelper)
+                setUpScrollListener()
                 setOnScrollChangeListener(scrollToPosition)
                 trailerView.scrollToPosition(scrollToPosition)
             }
@@ -55,14 +54,14 @@ class FeaturedView(context: Context?, attrs: AttributeSet? = null) : ConstraintL
         }
     }
 
-    private fun setUpScrollListener(snapHelper: PagerSnapHelper){
+    private fun setUpScrollListener() {
         trailerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
                 if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    if (currentView != snapHelper.findSnapView(linearLayout)) {
+                    if (currentView != snapHelper?.findSnapView(linearLayout)) {
                         adapter?.noVisibleView(currentView)
-                        currentView = snapHelper.findSnapView(linearLayout)
+                        currentView = snapHelper?.findSnapView(linearLayout)
                         onScrollFinished()
                     }
                 }
@@ -70,22 +69,26 @@ class FeaturedView(context: Context?, attrs: AttributeSet? = null) : ConstraintL
         })
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
     private fun setOnScrollChangeListener(position: Int) {
-        trailerView.setOnScrollChangeListener(
-                (OnScrollChangeListener { _, _, _, _, _ ->
-                    currentView = linearLayout?.findViewByPosition(position)
-                    onScrollFinished()
-                    trailerView.setOnScrollChangeListener(null)
-                }))
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            trailerView.setOnScrollChangeListener(
+                    (OnScrollChangeListener { _, _, _, _, _ ->
+                        currentView = linearLayout?.findViewByPosition(position)
+                        onScrollFinished()
+                        trailerView.setOnScrollChangeListener(null)
+                    }))
+        } else {
+            currentView = linearLayout?.findViewByPosition(position)
+            onScrollFinished()
+        }
     }
 
-    private fun onScrollFinished(){
+    private fun onScrollFinished() {
         adapter?.onScrollFinished(currentView)
     }
 
     private fun setUpAdapter(presentation: MoviesPresentation, moviePosterClickListener: MoviePosterClickListener) {
-        adapter = FeaturedMovieAdapter(presentation.data.second,moviePosterClickListener)
+        adapter = FeaturedMovieAdapter(presentation.data.second, moviePosterClickListener)
 
     }
 
